19a8df62a1d8fa17681b3a0049e8251d3133bb11
RTMaloney <ryan.maloney@york.ac.uk>
2018-09-27 17:15:21 +1000

    Code updated
    A bit more work on the plot

--- load_plot_bh_data.py
+++ load_plot_bh_data.py
@@ -5,11 +5,13 @@ Created on Wed Sep 26 11:12:06 2018
 @author: RyanDataPC
 """
 
+#%%
 import matplotlib as mpl 
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
+#%%
 # import the behavioural data excel sheet as a pandas dataframe.
 # We ask it to import the first sheet in the file, 'Trial by trial'
 
@@ -17,6 +19,7 @@ import numpy as np
 bh_data = pd.read_excel(r'C:\Users\RyanDataPC\Dropbox\DARPAK\material\Behavioural_Data.xlsx', 
                         sheet_name='Trial by trial')
 
+#%%
 # Pull out just the averages for plotting. They are in row 23
 # There are missing data for some subjects after trial 15, so we'll only plot those
 rewrd_trials = bh_data.iloc[23,1:16]   # The 15 reward trials first
@@ -26,16 +29,22 @@ instr_trials = bh_data.iloc[23,26:41]  # The 15 instructive trials
 rewrd_trials_se = bh_data.iloc[24,1:16]
 instr_trials_se = bh_data.iloc[24,26:41] 
 
+#%%
 # Now make the line plots in a new figure
 fig = plt.figure()
 # Convert performance data into a percentage contrast when plotting
-plt.plot(np.arange(1,16), rewrd_trials*100, color='r') 
-plt.plot(np.arange(1,16), instr_trials*100, color='k')
+plt.errorbar(np.arange(1,16), rewrd_trials*100, yerr = rewrd_trials_se*100, 
+             color = 'r')
+plt.errorbar(np.arange(1,16), instr_trials*100, yerr = instr_trials_se*100,
+             color = 'k')
 
 # Adjust the axis limits
-plt.xlim(0, 16)
+plt.xlim(0,16)
 plt.ylim(0,30)
 
-plt.errorbar(np.arange(1,16), rewrd_trials*100, yerr = rewrd_trials_se, color='r')
+# Adjust the tick labels on the x axis
+plt.xticks(np.arange(0,16, 5))
+
+
 
 
