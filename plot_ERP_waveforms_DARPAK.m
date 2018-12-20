038852a0e29de5b2ed86f14d1bb5a79bf45dffef
RTMaloney <ryan.maloney@york.ac.uk>
2018-12-20 12:30:02 +1100

    Created plotting function for ERP waveforms

---
+++ plot_ERP_waveforms_DARPAK.m
@@ -0,0 +1,11 @@
+
+
+clear 
+close all
+
+%Load the grand average data:
+ERP = pop_loaderp( 'filename', 'grandaverage_precsd.erp', 'filepath',...
+ 'C:\Users\RyanDataPC\Documents\Stefan Honours theses\DARPAK\data\erp\current\' );
+
+
+% Plot P3a amplitudes
