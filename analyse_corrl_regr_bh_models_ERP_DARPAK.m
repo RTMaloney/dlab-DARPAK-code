8396198a38c691bd391d800b5ef0ee11f61560d2
RTMaloney <ryan.maloney@york.ac.uk>
2019-04-26 18:53:22 +1000

    Update analyse_corrl_regr_bh_models_ERP_DARPAK.m

--- analyse_corrl_regr_bh_models_ERP_DARPAK.m
+++ analyse_corrl_regr_bh_models_ERP_DARPAK.m
@@ -90,7 +90,7 @@ end
 % For the modelling, we have subjects 4:25 (n=22).
 % For the ERPs, we drop 10, 15 and 20, giving us n=19.
 
-% NOTE: running the regressions with or without the mean reward variable does not change the 
+% NOTE: running the regressions with or without the mean reward variable does not change the
 % strength of the R2 values or their significance, but it does (obviously) change the Beta values
 
 %% P3as: regression
@@ -152,20 +152,19 @@ for chan = 1:num_electrodes
     % Omit the reward predictor from the model:
     %[b(:,chan), ~, ~, ~, stats(chan,:)] = regress(d',[x1, mean_update_size]);
     
+    % *** another way to do the regression (get p-values for each regressor):
+    lm = fitlm([x1, mean_reward, mean_update_size], d, 'linear') %> give t-stats for each regressor
+    %anova(lm,'summary') %> gives the f-test of the whole regression
+    
 end % end of loop across electrodes
 b=b';
-
-
-% *** another way to do the regression (get p-values for each regressor):
-%lm = fitlm([x1, mean_reward, mean_update_size], d(:,end), 'linear') > give t-stats for each regressor
-%anova(lm,'summary') > gives the f-test of the whole regression
 
 %% Now do LPPs amplitudes: regression with belief update size
 
 ALLERP = pop_geterpvalues( 'C:\Users\RyanDataPC\Documents\Stefan Honours theses\DARPAK\data\erp\current\DARPAK_all_subjects_list_consistent_bins_RM.txt', [ 550 900],...
-  5:6, [ 31 32 48] , 'Baseline', 'none', 'Binlabel', 'on', 'FileFormat', 'wide', 'Filename',...
- 'C:\Users\RyanDataPC\Documents\Stefan Honours theses\DARPAK\data\erp\current\mean_LPP_amplitude_all_subjects_550-900ms_epoch.txt', 'Fracreplace', 'NaN', 'InterpFactor',  1, 'Measure',...
- 'meanbl', 'PeakOnset',  1, 'Resolution',  3, 'SendtoWorkspace', 'on' );
+    5:6, [ 31 32 48] , 'Baseline', 'none', 'Binlabel', 'on', 'FileFormat', 'wide', 'Filename',...
+    'C:\Users\RyanDataPC\Documents\Stefan Honours theses\DARPAK\data\erp\current\mean_LPP_amplitude_all_subjects_550-900ms_epoch.txt', 'Fracreplace', 'NaN', 'InterpFactor',  1, 'Measure',...
+    'meanbl', 'PeakOnset',  1, 'Resolution',  3, 'SendtoWorkspace', 'on' );
 
 N = size(ERP_MEASURES,3);           % should be 19 subjects
 num_bins = size(ERP_MEASURES,1);
@@ -208,17 +207,20 @@ for chan = 1:num_electrodes
     fprintf(chan_names{chan})
     [b(:,chan), ~, ~, ~, stats(chan,:)] = regress(d',[x1, mean_reward, mean_update_size]);
     
+    % *** another way to do the regression (get p-values for each regressor):
+    lm = fitlm([x1, mean_reward, mean_update_size], d, 'linear') %> give t-stats for each regressor
+    %anova(lm,'summary') %> gives the f-test of the whole regression
+    
 end % end of loop across electrodes
 b=b';
 
-
 %% Regression of FRNs with * belief uncertainty *
-% Here we load the FRNs computed using the (modified) version 
-% of Daniel Bennett's code, getFRN.m 
+% Here we load the FRNs computed using the (modified) version
+% of Daniel Bennett's code, getFRN.m
 % Remember, the structure of the data is in the form of a 19 row * 30 column matrix
 % such that subjects are each in a different row, then electrodes in order 1:5, with each of the six BINS within an electrode
 % ie [electrode1([bin 1:6]), electode2([bin 1:6]), ...] forming the 6*5 =30 columns
-% So this is different to the above format of the other ERP components.
+% *** So this is different to the above format of the other ERP components ***.
 load 'C:\Users\RyanDataPC\Documents\Stefan Honours theses\DARPAK\data\erp\current\FRN_amplitudes_for_analysis.mat'
 
 num_bins = 2;
@@ -247,7 +249,7 @@ for chan = 1:num_electrodes
         mean_uncert = mean_uncert(ismember(4:25, IDs));
         
         d = frnAmplitude(:,5:6:end); % pull out instructive (bin 5) data only, for each electrode
-
+        
     elseif bn == 2
         mean_reward = nanmean(model_results.reward.mean_monetary,2); % values in dollars
         % Here, get rid of the subjects we are lacking in ERP data for.
@@ -255,13 +257,17 @@ for chan = 1:num_electrodes
         mean_uncert = nanmean(model_results.MI.mean_monetary, 2);
         mean_uncert = mean_uncert(ismember(4:25, IDs));
         
-        d = frnAmplitude(:,6:6:end); % pull out monetary (bin 6) data only, for each electrode      
+        d = frnAmplitude(:,6:6:end); % pull out monetary (bin 6) data only, for each electrode
     end
     
     % Perform the regression for this electrode:
     % Remember, the vector stats contains the R2 statistic, the F-statistic and its p-value, and an estimate of the error variance.
     fprintf(chan_names{chan})
     [b(:,chan), ~, ~, ~, stats(chan,:)] = regress(d(:,chan),[x1, mean_reward, mean_uncert]);
+    
+    % *** another way to do the regression (get p-values for each regressor):
+    lm = fitlm([x1, mean_reward, mean_uncert], d(:,chan), 'linear') %> give t-stats for each regressor
+    %anova(lm,'summary') %> gives the f-test of the whole regression
     
 end % end of loop across electrodes
 b=b';
